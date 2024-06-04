529734563279318b477d8adf8a97a8d734ab4175
DrGrenkenstien <shubhambhadoria10@gmail.com>
2024-06-04 12:13:38 +0530

    added utility functions

--- Utitlity_functions/numbers_with_j_bit_set.cpp
+++ Utitlity_functions/numbers_with_j_bit_set.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 
 // returns count of number between 0 and n with jth bit set
+// link for reference: https://atcoder.jp/contests/abc356/editorial/10144
 long long f(long long j,long long n){
   long long p2=(1ll<<j); // 2^j
   long long k=n/(2*p2);
